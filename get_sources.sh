84978f9a0c85bf347f77daf1116298ac080aa99a
Arkady L. Shane <ashejn@gmail.com>
2011-04-26 19:07:38 +0400

    update to Fedora 15.29

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-15.27.tar.bz2/8471374e1546420ed7e235d2b897acd0/anaconda-15.27.tar.bz2
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-15.29.tar.bz2/5c8a24804f8622544614c11f43d407ff/anaconda-15.29.tar.bz2
 
