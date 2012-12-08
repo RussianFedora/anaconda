1ed9597fd7748e628f94023a97617919f4ec14da
Tigro <ashejn@gmail.com>
2012-12-09 02:59:51 +0400

    update to 18.37

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-18.29.2.tar.bz2/00f9597bd8d943fe87e118d8e0ce24e8/anaconda-18.29.2.tar.bz2
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-18.37.tar.bz2/c1b5b78eda4a0ad3982fe5fa3aeae57e/anaconda-18.37.tar.bz2
 
