23ab58a38b274e597f005a4eeae1d631a6eb3d94
Tigro <ashejn@gmail.com>
2014-03-05 15:06:22 +0400

    update to 21.25

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-20.25.16.tar.bz2/91897a4e47fa2e1b0bc9376455c23265/anaconda-20.25.16.tar.bz2
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-21.25.tar.bz2/fe8cdf4ebdb3eb7cd4fbbc785d5a657a/anaconda-21.25.tar.bz2
 
