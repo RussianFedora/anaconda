b64d1659eb3f1c5b41a272e64ca5168a378ff9a1
Tigro <ashejn@gmail.com>
2016-03-16 17:54:56 +0300

    update to 24.13.1

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-23.19.10.tar.bz2/03d4b336be46df720f808783b6d71da3/anaconda-23.19.10.tar.bz2
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-24.13.1.tar.bz2/7cd1ec8995a8b7fb0a0d52c7e59142bd/anaconda-24.13.1.tar.bz2
 
