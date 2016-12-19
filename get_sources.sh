c54083bd6a2b0a8c5456171e666f049ace8de9e7
Tigro <ashejn@gmail.com>
2016-12-19 16:37:30 +0300

    update to 25.20.9

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-25.20.8.tar.bz2/3b38b929c86b595160d9375d14f05b94/anaconda-25.20.8.tar.bz2
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/anaconda/anaconda-25.20.9.tar.bz2/sha512/423016684eb713bc5330f1e844a2defb2668f8dd9145fd149f2a612c8fa6b6b94c121901d25f5156ec9a25250e848c4e3f2d2ce7c0abc3458eaf41c94bf0792a/anaconda-25.20.9.tar.bz2
 
