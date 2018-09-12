32ef89d0d33c80aed7b27f726ccc85e4875abaee
Tigro <ashejn@gmail.com>
2018-09-12 22:59:43 +0300

    update to 29.24.2

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO https://src.fedoraproject.org/repo/pkgs/anaconda/anaconda-28.22.11.tar.bz2/sha512/f79bd71f472fa09df6696874f581635bf48beb78abc1ef7bb774c61dd163992ff17cb871844fab0990de99afb7e787055346262d6b61349fc842e9ba6e78b5f3/anaconda-28.22.11.tar.bz2
+curl -sO https://src.fedoraproject.org/repo/pkgs/anaconda/anaconda-29.24.2.tar.bz2/sha512/34458d28111fdb0a433475289e5e57e80afc69707fbd70cc6d9d28d38d2b2deeb9ca1446cd70aef7ae82a0a0a1cc259f700ecdb676a44c4d57944da81c784549/anaconda-29.24.2.tar.bz2
 
